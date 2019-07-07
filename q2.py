4faacb2f88180e81d050ba0c4494da8b62197e08
6f8l <atsuki02jojo@gmail.com>
2019-07-07 18:53:51 +0900

    Solve q2 by using list

--- q2.py
+++ q2.py
@@ -1,5 +1,18 @@
 def main():
-    return
+    s = 'パタトクカシーー'
+    arr = list(s)
+    ans_arr = []
+    for i in range(len(arr)):
+        if i % 2 == 0:
+            pass
+        elif i % 2 == 1:
+            ans_arr.append(arr[i])
+        else:
+            print('error')
+
+    ans = ''.join(ans_arr)
+    print(ans)
+    return ans
 
 if __name__=='__main__':
     main()
