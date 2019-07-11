7c94dde6ee35fed7150c2614150f55f7577d667c
6f8l <atsuki02jojo@gmail.com>
2019-07-11 21:56:55 +0900

    solve q10

--- q10.py
+++ q10.py
@@ -3,20 +3,24 @@ import random
 sentence = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 
 def execute(sentence):
-    """
-    スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ
-    ただし，長さが４以下の単語は並び替えないこととする.
-    適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."）を与えその実行結果を確認せよ．
-    """
     arr = sentence.split(' ')
+    result_arr = []
     for word in arr:
         word_len = len(word)
         if word_len >= 4:
             init_str = word[0:1]
             final_str = word[word_len - 1:word_len]
-            # for i in range()
+            other_strs = word[1:word_len - 1]
+            other_strs_arr = random.sample(other_strs, len(other_strs))
+            word = init_str + ''.join(other_strs_arr) + final_str
+            result_arr.append(word)
         else:
-            pass
+            result_arr.append(word)
+        # append space to devide sentence by space.
+        result_arr.append(' ')
 
+    result_str = ''.join(result_arr)
+    print(result_str)
+    return result_str
 
 execute(sentence)
